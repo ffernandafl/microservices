7de721deeedb93c486765f0e0afdb5d2dc1868e4
Fernanda Flores (iDEAS-Cloud Transformation) <fe20320617@wipro.com>
2022-04-12 21:29:54 -0500

    eureka

--- accesories-service/src/main/java/com/accesories/service/AccesoriesServiceApplication.java
+++ accesories-service/src/main/java/com/accesories/service/AccesoriesServiceApplication.java
@@ -2,8 +2,10 @@ package com.accesories.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AccesoriesServiceApplication {
 
 	public static void main(String[] args) {
